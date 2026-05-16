fd369606cb65cffe4bc6a814ab4f245ea15f486a
Geoecho <hbristik@gmail.com>
2026-05-16 12:04:12 +0200

    / olish and bout s text updates

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -18,7 +18,7 @@ export function AboutUs() {
             About Us
           </h2>
           <p className="font-sans text-lg md:text-xl font-medium leading-relaxed text-muted-foreground">
-            We created The Big Impact platform with a lot of devotion towards support and recgonition of the youth. We aim to make a meaningful impact across every dimension of youth life, from education and leadership to culture,art,  innovation, and personal growth. In a time where uncertainty defines the present we stand as a platform that provides direction,recognition, opportunity, and transformation.We give young people exactly what they need most - the right platform to rise. Our goal is to shape the present by building a youth empowering platform where young people grow, lead, and create meaningful impact.
+            We created The Big Impact platform with a lot of devotion towards support and recognition of the youth. We aim to make a meaningful impact across every dimension of youth life, from education and leadership to culture, art, innovation and personal growth. In a time where uncertainty defines the present we stand as a platform that provides direction, recognition, opportunity and transformation. We give young people exactly what they need most - the right platform to rise. Our goal is to shape the present by building a youth empowering platform where young people grow, lead and create meaningful impact.
           </p>
         </motion.div>
       </div>
